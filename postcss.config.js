daf5a5253a3fb723cc6960133a01456069d507a5
NEHA MUSHTAQ <neha75010@yahoo.com>
2022-11-26 13:17:12 +0100

    created navbar and set colors

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    plugins: {
+        // include whatever plugins you want
+        //but make sure you install these via yarn or npm !
+
+        //add browserlist config to package.json (see below)
+        autoprefixer: {}
+    }
+}
